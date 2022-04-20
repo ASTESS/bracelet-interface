de0a1ba7a1f219f563653cf74b878fc061da7746
Matthias Van de Sande <anthony.stessels1@gmail.com>
2022-04-20 22:47:33 +0200

    Working search bar Order by selected heading

--- src/Pages/OverView.js
+++ src/Pages/OverView.js
@@ -1,7 +1,9 @@
 import {useEffect, useState} from "react";
 
 export default function OverView(){
-    const [data, setData] = useState([{name: "name", totalFalls: 10, fallsToday: 5, fallsYesterday: 2, batteryValue: 90}, {name: "name", totalFalls: 15, fallsToday: 5, fallsYesterday: 2, batteryValue: 90}, {name: "name", totalFalls: 10, fallsToday: 5, fallsYesterday: 2, batteryValue: 90}]);
+    const [data, setData] = useState([{name: "name", totalFalls: 25, fallsToday: 5, fallsYesterday: 2, batteryValue: 90}, {name: "test", totalFalls: 20, fallsToday: 1, fallsYesterday: 13, batteryValue: 75}, {name: "bob", totalFalls: 10, fallsToday: 5, fallsYesterday: 2, batteryValue: 95}]);
+    const [search, setSearch] = useState("");
+    const [sort, setSort] = useState("Name");
 
     useEffect(() => {
 
@@ -14,37 +16,61 @@ export default function OverView(){
         */
     }, []);
 
+    function sortBy(sortBy){
+        setSort(sortBy);
+    }
+
 
     return(
         <div>
             <h1>OverView</h1>
 
-
             <div className="search-box" style={{float: "right", padding: 10}}>
-                <input type="text" placeholder="Search" />
-                <button>Search</button>
+                <input type="text" placeholder="Search" onChange={(event) => setSearch(event.target.value)}/>
             </div>
 
             <table style={{width: "100%"}}>
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Total Falls</th>
-                        <th>Falls Today</th>
-                        <th>Falls Yesterday</th>
-                        <th>Battery Value</th>
+                        <th onClick={() => sortBy("Name")}>Name</th>
+                        <th onClick={() => sortBy("Total Falls")}>Total Falls</th>
+                        <th onClick={() => sortBy("Falls Today")}>Falls Today</th>
+                        <th onClick={() => sortBy("Falls Yesterday")}>Falls Yesterday</th>
+                        <th onClick={() => sortBy("Battery Value")}>Battery Value</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map(item => (
-                        <tr key={item.name}>
-                            <td>{item.name}</td>
-                            <td>{item.totalFalls}</td>
-                            <td>{item.fallsToday}</td>
-                            <td>{item.fallsYesterday}</td>
-                            <td>{item.batteryValue}</td>
-                        </tr>
-                    ))}
+
+
+                {data.sort((a, b) => {
+                    if(sort === "Name"){
+                        return a.name.localeCompare(b.name);
+                    }
+                    else if(sort === "Total Falls"){
+                        return a.totalFalls - b.totalFalls;
+                    }
+                    else if(sort === "Falls Today"){
+                        return a.fallsToday - b.fallsToday;
+                    }
+                    else if(sort === "Falls Yesterday"){
+                        return a.fallsYesterday - b.fallsYesterday;
+                    }
+                    else if(sort === "Battery Value"){
+                        return a.batteryValue - b.batteryValue;
+                    }
+                }).map((item, index) => {
+                    if(item.name.toLowerCase().includes(search.toLowerCase())){
+                        return(
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>{item.totalFalls}</td>
+                                <td>{item.fallsToday}</td>
+                                <td>{item.fallsYesterday}</td>
+                                <td>{item.batteryValue}</td>
+                            </tr>
+                        );
+                    }
+                })}
                 </tbody>
             </table>
         </div>
