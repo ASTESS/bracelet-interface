1d8dedacd15e3d0af55894c6bf186e075db46101
Matthias Van de Sande <anthony.stessels1@gmail.com>
2022-04-23 21:42:45 +0200

    Login + signup

---
+++ src/Components/SignUp.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {Alert, Backdrop, Box, Button, Card, IconButton, Snackbar, TextField} from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+
+export default function SignUp(props) {
+
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [confirmPassword, setConfirmPassword] = React.useState('');
+    const [firstName, setFirstName] = React.useState('');
+    const [phone, setPhone] = React.useState('');
+
+    const [passwordDontMatch, setPasswordDontMatch] = React.useState(false);
+
+    function signUp(){
+        if(password === confirmPassword){
+            fetch('http://localhost:3333/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: new URLSearchParams({
+                    "email": email,
+                    "password": password,
+                    "firstName": firstName,
+                    //"phone": phone
+                }).toString()
+            }).then(r => r.json()).then(data => {
+                console.log(data);
+            })
+        }else{
+            setPasswordDontMatch(true);
+        }
+
+    }
+
+    return(
+        <Backdrop open>
+            <Card style={{width: "50%"}}>
+
+                <IconButton style={{float: "right"}} onClick={() => props.toggle()}>
+                    <CloseIcon/>
+                </IconButton>
+
+                <h1>Sign Up</h1>
+                <Box sx={{display: "flex", flexDirection: "column"}} style={{paddingTop: 10}}>
+                    <TextField
+                        style={{width: "90%", alignSelf: "center"}}
+                        label="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        type={'password'}
+                        style={{width: "90%", alignSelf: "center", marginTop: 20}}
+                        label="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <TextField
+                        type={'password'}
+                        style={{width: "90%", alignSelf: "center", marginTop: 20}}
+                        label="Confirm Password"
+                        value={confirmPassword}
+                        onChange={e => setConfirmPassword(e.target.value)}
+                    />
+                    <TextField
+                        style={{width: "90%", alignSelf: "center", marginTop: 20}}
+                        label="First Name"
+                        value={firstName}
+                        onChange={e => setFirstName(e.target.value)}
+                    />
+                    <TextField
+                        style={{width: "90%", alignSelf: "center", marginTop: 20}}
+                        label="Phone"
+                        value={phone}
+                        onChange={e => setPhone(e.target.value)}
+                    />
+                </Box>
+                <Button onClick={() => signUp()} style={{margin: 10}} variant={"contained"}>Sign up</Button>
+            </Card>
+            <Snackbar open={passwordDontMatch} autoHideDuration={10000} onClose={() => setPasswordDontMatch(false)}>
+                <Alert severity="error">Password's does not match</Alert>
+            </Snackbar>
+
+        </Backdrop>
+    )
+}
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,19 +1,63 @@
+import {useState} from "react";
+import {Box, Button, Card, TextField, Typography} from "@mui/material";
+import SignUp from "../Components/SignUp";
 
 export default function Login(){
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <label>
-          Email:
-          <input type="text" name="email" />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [showSingUp, setShowSingUp] = useState(false);
+
+    function toggleSingUp(){
+        setShowSingUp(!showSingUp);
+    }
+
+
+    function login(){
+        fetch('http://localhost:3333/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: new URLSearchParams({
+                "email": email,
+                "password": password
+            }).toString()
+        }).then(r => r.json()).then(data => {
+            console.log(data);
+        })
+    }
+
+
+    return (
+        <div>
+            <Card style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', width: "30%"}}>
+                <Box sx={{display: "flex", flexDirection: "column"}} style={{paddingTop: 10}}>
+                    <TextField
+                        style={{width: "90%", alignSelf: "center"}}
+                        label="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        type={'password'}
+                        style={{width: "90%", alignSelf: "center", marginTop: 20}}
+                        label="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                </Box>
+                <Box sx={{display: 'flex', alignItems: 'center', pl: 1, pb: 1}}>
+                    <Typography onClick={() => toggleSingUp()} style={{paddingLeft: 10, flex: 1, textDecoration: "underline"}}>
+                        Register
+                    </Typography>
+                    <Button style={{margin: 20, flex: 1}} variant={"contained"} onClick={login}>Login</Button>
+                </Box>
+            </Card>
+
+            {showSingUp ? <SignUp toggle={toggleSingUp}/> : null}
+        </div>
+
+
+    );
 }
