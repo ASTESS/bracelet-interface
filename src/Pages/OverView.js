771ce17bd676ef0c66ccc1739baadf43e5ebcd1e
Matthias Van de Sande <anthony.stessels1@gmail.com>
2022-04-25 00:26:19 +0200

    Descending + Ascending

--- src/Pages/OverView.js
+++ src/Pages/OverView.js
@@ -12,6 +12,7 @@ import {
 } from "@mui/material";
 
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 export default function OverView(){
     const [data, setData] = useState([{name: "name", totalFalls: 25, fallsToday: 5, fallsYesterday: 2, batteryValue: 90}, {name: "test", totalFalls: 20, fallsToday: 1, fallsYesterday: 13, batteryValue: 75}, {name: "bob", totalFalls: 10, fallsToday: 5, fallsYesterday: 2, batteryValue: 95}]);
@@ -42,7 +43,13 @@ export default function OverView(){
     }, []);
 
     function sortBy(sortBy){
-        setSort(sortBy);
+        if(sort.endsWith("Asc")){
+            setSort(sortBy + " Desc");
+        }else if(sort.endsWith("Desc")){
+            setSort(sortBy + " Asc");
+        }else{
+            setSort(sortBy + " Asc");
+        }
     }
     return(
         <div>
@@ -78,47 +85,72 @@ export default function OverView(){
                             <TableCell>
                                 <Box sx={{display: 'flex', alignItems: 'center', pl: 1, pb: 1}}>
                                 <Button onClick={() => sortBy("Name")}>Name</Button>
-                                {sort === "Name" ? <ArrowDropDownIcon/> : null}
+                                    {(sort.includes("Name") && sort.endsWith("Asc")) ? <ArrowDropUpIcon/> : null}
+                                    {(sort.includes("Name") && sort.endsWith("Desc")) ? <ArrowDropDownIcon/> : null}
                                 </Box>
                             </TableCell>
                             <TableCell>
                                 <Box sx={{display: 'flex', alignItems: 'center', pl: 1, pb: 1}}>
                                 <Button onClick={() => sortBy("Total Falls")}>Total Falls</Button>
-                                {sort === "Total Falls" ? <ArrowDropDownIcon/> : null}
+                                    {(sort.includes("Total Falls") && sort.endsWith("Asc")) ? <ArrowDropUpIcon/> : null}
+                                    {(sort.includes("Total Falls") && sort.endsWith("Desc")) ? <ArrowDropDownIcon/> : null}
                                 </Box>
                             </TableCell>
                             <TableCell>
                                 <Box sx={{display: 'flex', alignItems: 'center', pl: 1, pb: 1}}>
                                 <Button onClick={() => sortBy("Falls Today")}>Falls Today</Button>
-                                {sort === "Falls Today" ? <ArrowDropDownIcon/> : null}
+                                    {(sort.includes("Falls Today") && sort.endsWith("Asc")) ? <ArrowDropUpIcon/> : null}
+                                    {(sort.includes("Falls Today") && sort.endsWith("Desc")) ? <ArrowDropDownIcon/> : null}
                                 </Box>
                             </TableCell>
                             <TableCell>
                                 <Box sx={{display: 'flex', alignItems: 'center', pl: 1, pb: 1}}>
                                 <Button onClick={() => sortBy("Falls Yesterday")}>Falls Yesterday</Button>
-                                {sort === "Falls Yesterday" ? <ArrowDropDownIcon/> : null}
+                                    {(sort.includes("Falls Yesterday") && sort.endsWith("Asc")) ? <ArrowDropUpIcon/> : null}
+                                    {(sort.includes("Falls Yesterday") && sort.endsWith("Desc")) ? <ArrowDropDownIcon/> : null}
                                 </Box>
                             </TableCell>
                             <TableCell>
                                 <Box sx={{display: 'flex', alignItems: 'center', pl: 1, pb: 1}}>
                                 <Button onClick={() => sortBy("Battery Value")}>Battery Value</Button>
-                                {sort === "Battery Value" ? <ArrowDropDownIcon/> : null}
+                                    {(sort.includes("Battery Value") && sort.endsWith("Asc")) ? <ArrowDropUpIcon/> : null}
+                                    {(sort.includes("Battery Value") && sort.endsWith("Desc")) ? <ArrowDropDownIcon/> : null}
                                 </Box>
                             </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {data.filter(row => row.name.toLowerCase().includes(search.toLowerCase())).sort((a, b) => {
-                            if(sort === "Name"){
-                                return a.name.localeCompare(b.name);
-                            }else if(sort === "Total Falls"){
-                                return a.totalFalls - b.totalFalls;
-                            }else if(sort === "Falls Today"){
-                                return a.fallsToday - b.fallsToday;
-                            }else if(sort === "Falls Yesterday"){
-                                return a.fallsYesterday - b.fallsYesterday;
-                            }else if(sort === "Battery Value"){
-                                return a.batteryValue - b.batteryValue;
+                            if(sort.includes("Name")){
+                                if(sort.endsWith("Desc")){
+                                    return a.name.localeCompare(b.name);
+                                }else if(sort.endsWith("Asc")){
+                                    return b.name.localeCompare(a.name);
+                                }
+                            }else if(sort.includes("Total Falls")){
+                                if(sort.endsWith("Desc")){
+                                    return b.totalFalls - a.totalFalls;
+                                }else if(sort.endsWith("Asc")){
+                                    return a.totalFalls - b.totalFalls;
+                                }
+                            }else if(sort.includes("Falls Today")){
+                                if(sort.endsWith("Desc")){
+                                    return b.fallsToday - a.fallsToday;
+                                }else if(sort.endsWith("Asc")){
+                                    return a.fallsToday - b.fallsToday;
+                                }
+                            }else if(sort.includes("Falls Yesterday")){
+                                if(sort.endsWith("Desc")){
+                                    return b.fallsYesterday - a.fallsYesterday;
+                                }else if(sort.endsWith("Asc")){
+                                    return a.fallsYesterday - b.fallsYesterday;
+                                }
+                            }else if(sort.includes("Battery Value")){
+                                if(sort.endsWith("Desc")){
+                                    return b.batteryValue - a.batteryValue;
+                                }else if(sort.endsWith("Asc")){
+                                    return a.batteryValue - b.batteryValue;
+                                }
                             }
                         }).map(row => (
                             <TableRow key={row.name}>
